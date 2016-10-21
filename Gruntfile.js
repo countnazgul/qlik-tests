2853467b9488915686b51886b8b22518f8fc9255
Stefan Stoichev <stefan.stoichev@gmail.com>
2016-10-21 12:16:05 +0100

    Update Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,5 @@
-module.exports = function (grunt) {
+module.exports = function(grunt) {
 
-	//files = [];
 	targetDir = '';
 
 	grunt.initConfig({
@@ -9,16 +8,12 @@ module.exports = function (grunt) {
 				explicitArray: false,
 			},
 			xml2json: {
-				files: [
-					{
-						expand: true,
-						//flatten: true,
-						//cwd: '<%= source %>',
-						src: '<%= files %>',
-						dest: '',
-						ext: '.json'
-					}
-				]
+				files: [{
+					expand: true,
+					src: '<%= files %>',
+					dest: '',
+					ext: '.json'
+				}]
 			},
 		},
 		mkdir: {
@@ -29,21 +24,19 @@ module.exports = function (grunt) {
 			},
 		},
 		clean: {
-			folder: '<%= targetDir %>'
+			folder: './files'
 		},
 		copy: {
 			main: {
-				files: [
-					{
-						expand: true,
-						cwd: '<%= source %>',
-						src: '<%= filesToCopy %>',
-						dest: '<%= targetDir1 %>',
-						filter: 'isFile'
-					},
-				],
+				files: [{
+					expand: true,
+					cwd: '<%= source %>',
+					src: '<%= filesToCopy %>',
+					dest: '<%= targetDir1 %>',
+					filter: 'isFile'
+				}],
 			},
-		},
+		}
 	});
 
 	function makeid() {
@@ -61,71 +54,45 @@ module.exports = function (grunt) {
 	grunt.loadNpmTasks('grunt-mkdir');
 	grunt.loadNpmTasks('grunt-contrib-copy');
 
-	grunt.registerTask('celanTempDir', '', function () {
-		grunt.config.set('targetDir', '9ra4i');
+	grunt.registerTask('default', '', function() {
+
 		grunt.task.run('clean');
-	});
-
-	grunt.registerTask('tempDir', '', function () {
-		targetDir = makeid();
-		console.log(targetDir)
-		grunt.config.set('targetDir', [targetDir]);
-
-		//files = ['DocProperties.xml', 'AllProperties.xml'];
-		grunt.task.run('mkdir');
-	});
-
-	grunt.registerTask('convert1', '', function () {
-
-		//files = ['./Eu6V9/DocProperties.xml', './Eu6V9/AllProperties.xml'];
-		//grunt.config.set('files', files);
-
-		//files = ['DocProperties.xml', 'AllProperties.xml'];
-		//grunt.task.run('convert');
-	});
-
-	grunt.registerTask('default', '', function () {
-
 		source = grunt.option('source');
-		console.log(source)
-		//source = source.replace(/\\/g, '/');
-		//source = '/' + source.replace(/:/g, ':');
-		//console.log(source)
-		//source = 'C:/Users/Home/Documents/test1/test-prj';
 
-		targetDir = makeid();
-		//targetDir = 'JM2AQ'
+		if (source) {
 
-		grunt.config.set('targetDir', ['files/' + targetDir]); //[targetDir]
-		grunt.config.set('targetDir1', 'files/' + targetDir);
-		grunt.config.set('source', source);
+			targetDir = makeid();
 
-		files = ['./' + targetDir + '/DocProperties.xml', './' + targetDir + '/AllProperties.xml'];
-		filesToCopy = ['DocProperties.xml', 'AllProperties.xml', 'LoadScript.txt', 'Module.txt'];
+			grunt.config.set('targetDir', ['files/' + targetDir]);
+			grunt.config.set('targetDir1', 'files/' + targetDir);
+			grunt.config.set('source', source);
 
-		grunt.config.set('filesToCopy', filesToCopy);
-		grunt.config.set('files', files);
+			files = ['files/' + targetDir + '/DocProperties.xml', 'files/' + targetDir + '/AllProperties.xml'];
+			filesToCopy = ['DocProperties.xml', 'AllProperties.xml', 'LoadScript.txt', 'Module.txt'];
 
-		grunt.task.run('mkdir');
-		grunt.task.run('copy');
-		grunt.task.run('convert');
-		grunt.task.run('localPath');
-		grunt.task.run('macroOnOpen');
-		grunt.task.run('macroOnPostReload');
-		grunt.task.run('macroModule');
-		grunt.task.run('generateLogFile');
-		grunt.task.run('synKeys');
-		//grunt.task.run('clean');
+			grunt.config.set('filesToCopy', filesToCopy);
+			grunt.config.set('files', files);
+
+
+			grunt.task.run('mkdir');
+			grunt.task.run('copy');
+			grunt.task.run('convert');
+			grunt.task.run('localPath');
+			grunt.task.run('macroOnOpen');
+			grunt.task.run('macroOnPostReload');
+			grunt.task.run('macroModule');
+			grunt.task.run('generateLogFile');
+			grunt.task.run('synKeys');
+			grunt.task.run('force:clean');
+		}
+		else {
+			grunt.fail.fatal('No source folder provided! Pleaase start the test with --source=MyDir parameter.');
+		}
 
 	});
 
-	grunt.registerTask('test', '', function () {
-		var target = grunt.option('myParam');
-		console.log(t());
-	});
-
-	grunt.registerTask('synKeys', '', function () {
-		mapping = grunt.file.readJSON('./' + targetDir + '/AllProperties.json');
+	grunt.registerTask('synKeys', '', function() {
+		mapping = grunt.file.readJSON('files/' + targetDir + '/AllProperties.json');
 		fields = mapping.AllProperties.FieldProperties.FieldProperties;
 
 		synKeyCount = 0;
@@ -138,82 +105,97 @@ module.exports = function (grunt) {
 		}
 
 		if (synKeyCount > 0) {
-			grunt.fail.warn(synKeyCount + ' synthetic key(s) found!')
-		} else {
-			grunt.log.ok('No synthetic keys');
+			grunt.fail.warn(synKeyCount + ' synthetic key(s) found!');
+		}
+		else {
+			grunt.log.ok('No synthetic keys').ok();
 		}
 
 	});
 
-	grunt.registerTask('localPath', '', function () {
-		script = grunt.file.read('./' + targetDir + '/LoadScript.txt');
+	grunt.registerTask('localPath', '', function() {
+		script = grunt.file.read('files/' + targetDir + '/LoadScript.txt');
 		count = (script.match(/:\\/g) || []).length;
 
 		if (count > 0) {
-			grunt.fail.warn('Local path used in the script!')
-		} else {
-			grunt.log.ok('No local paths in script')
+			grunt.fail.warn('Local path used in the script!');
+		}
+		else {
+			grunt.log.ok('No local paths in script').ok();
 		}
 	});
 
-	grunt.registerTask('macroOnOpen', '', function () {
-		mapping = grunt.file.readJSON('./' + targetDir + '/DocProperties.json');
+	grunt.registerTask('macroOnOpen', '', function() {
+		mapping = grunt.file.readJSON('files/' + targetDir + '/DocProperties.json');
 		onOpenActions = mapping.DocProperties.OnOpenActionItems.ActionItemDef;
 		hasMacro = false;
 
-		for (var i = 0; i < onOpenActions.length; i++) {
-			actionItem = onOpenActions[i];
+		if (onOpenActions) {
+			for (var i = 0; i < onOpenActions.length; i++) {
+				actionItem = onOpenActions[i];
 
-			if (actionItem.Type == 'TYPE_MACRO') {
-				hasMacro = true;
+				if (actionItem.Type == 'TYPE_MACRO') {
+					hasMacro = true;
+				}
+			}
+
+			if (hasMacro == true) {
+				grunt.fail.warn('Macro module OnOpen!');
+			}
+			else {
+				grunt.log.ok('No macro OnOpen').ok();
 			}
 		}
-
-		if (hasMacro = true) {
-			grunt.fail.warn('Macro module OnOpen!')
-		} else {
-			grunt.log.ok('No macro OnOpen')
+		else {
+			grunt.log.ok('No macro OnOpen').ok();
 		}
 	});
 
-	grunt.registerTask('macroOnPostReload', '', function () {
-		mapping = grunt.file.readJSON('./' + targetDir + '/DocProperties.json');
+	grunt.registerTask('macroOnPostReload', '', function() {
+		mapping = grunt.file.readJSON('files/' + targetDir + '/DocProperties.json');
 		onPostReloadActions = mapping.DocProperties.OnPostReloadActionItems.ActionItemDef;
 		hasMacro = false;
 
-		for (var i = 0; i < onPostReloadActions.length; i++) {
-			actionItem = onPostReloadActions[i];
+		if (onPostReloadActions) {
+			for (var i = 0; i < onPostReloadActions.length; i++) {
+				actionItem = onPostReloadActions[i];
 
-			if (actionItem.Type == 'TYPE_MACRO') {
-				hasMacro = true;
+				if (actionItem.Type == 'TYPE_MACRO') {
+					hasMacro = true;
+				}
+			}
+
+			if (hasMacro == true) {
+				grunt.fail.warn('Macro module OnPostReload!');
+			}
+			else {
+				grunt.log.ok('No macro OnPostReload').ok();
 			}
 		}
-
-		if (hasMacro = true) {
-			grunt.fail.warn('Macro module OnPostReload!')
-		} else {
-			grunt.log.ok('No macro OnPostReload')
+		else {
+			grunt.log.ok('No macro OnPostReload').ok();
 		}
 	});
 
-	grunt.registerTask('macroModule', '', function () {
-		module = grunt.file.read('./' + targetDir + '/Module.txt')
+	grunt.registerTask('macroModule', '', function() {
+		module = grunt.file.read('files/' + targetDir + '/Module.txt');
 		if (module.length > 0) {
 			grunt.fail.warn('Macro module is used!');
-		} else {
-			grunt.log.ok('Macro module is empty');
+		}
+		else {
+			grunt.log.ok('Macro module is empty').ok();
 		}
 	});
 
-	grunt.registerTask('generateLogFile', '', function () {
-		mapping = grunt.file.readJSON('./' + targetDir + '/DocProperties.json');
+	grunt.registerTask('generateLogFile', '', function() {
+		mapping = grunt.file.readJSON('files/' + targetDir + '/DocProperties.json');
 		generateLogFile = mapping.DocProperties.GenerateLogfile;
 
 		if (generateLogFile == "false") {
-			grunt.fail.warn('Log file is NOT going to be generated after reload!')
-		} else {
-			grunt.log.ok('Log file will be generated upon reload')
+			grunt.fail.warn('Log file is NOT going to be generated after reload!');
+		}
+		else {
+			grunt.log.ok('Log file will be generated upon reload').ok();
 		}
 	});
-
 };
